04c07a322826c84643ec2b229f50a2ec666ae29b
evgeny.izvekov <evgeny.izvekov@redcollar.ru>
2026-08-26 15:21:56 +0300

    deleted unnecessary files

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,9 +1,6 @@
 # ruff: noqa: E501
 from .base import *  # noqa: F403
-from .base import DATABASES
-from .base import INSTALLED_APPS
-from .base import REDIS_URL
-from .base import env
+from .base import DATABASES, INSTALLED_APPS, REDIS_URL, env
 
 # GENERAL
 # ------------------------------------------------------------------------------
