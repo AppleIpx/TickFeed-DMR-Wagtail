c64d8609a4223c829ead0c7a617deec0e386cb39
evgeny.izvekov <evgeny.izvekov@redcollar.ru>
2026-09-22 16:52:31 +0300

    feat: added base fronted

---
+++ frontend/vite.config.ts
@@ -0,0 +1,22 @@
+import { fileURLToPath, URL } from "node:url";
+
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  server: {
+    host: "0.0.0.0",
+    port: 3000,
+    strictPort: true,
+    watch: {
+      usePolling: true,
+      interval: 300,
+    },
+  },
+});
